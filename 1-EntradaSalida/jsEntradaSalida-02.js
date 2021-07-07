05b832a0ca8d5330b67c53d92c99cb8ac3493ab3
IanIwanczuk <a345587553455875@gmail.com>
2021-07-07 07:31:53 -0300

    SEGUNDO COMMIT

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,23 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
 function mostrar()
 {
-	alert("ok");
+	// "alert" se usa para mostrar una ventana que muestra un mensaje.
+	// "let" se utiliza para determinar variables y "cons" también.
+
+	let nombre;
+
+
+	// Se pone ";" al final de las lineas como si fuese un punto.
+	// También se podría escribir: let nombre = "Xian".
+
+	// nombre = "Xian";
+
+	
+	nombre = prompt("Ingrese su nombre:");
+	alert("Tu nombre es " + nombre)
+
 }
 
