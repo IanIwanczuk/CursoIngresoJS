d2e37a4f4729b4a75b32c6872f70c026b9d30b08
IanIwanczuk <a345587553455875@gmail.com>
2021-07-31 20:07:39 -0300

    Practica para el examen eheheahaheahehaehaeheah

--- 9-Parciales/2019_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,58 @@
 function mostrar()
 {
+    /* 
+    Realizar el algoritmo que permita el ingreso de 10 bolsas de alimento por prompt,con los kilos (validar entre
+    0 y 500) , sabor validar(carne vegetales pollo) e informar por alert:
+        a) El promedio de los kilos totales.
+        b) El mas liviano y su sabor
+        c) La cantidad de sabor carne y el promedio de kilos de sabor carne
+    */
+
+    let kilos; 
+    let tipoCompra;
+    let acumKilos = 0;
+    let promedioKilos;
+    let masLivianoKilos;
+    let masLivianoTipo;
+    let flagMasLiviano = 1;
+    let acumKilosCarne = 0;
+    let contadorCarne = 0;
+    let promedioKilosCarne = 0; 
+
+    for(let i = 1; i <= 10; i++){
+        kilos = parseFloat(prompt("Ingrese los kilos de esta bolsa. (0-500 Kilos)"));
+        while(isNaN(kilos) || !(kilos >= 0 && kilos <= 500)){
+            kilos = parseFloat(prompt("Por favor, ingrese un peso válido. (0-500 Kilos)"));
+        }
+        acumKilos += kilos;
+
+        tipoCompra = prompt("Indique el producto comprado. (Carne/Vegetales/Pollo)").toLowerCase();
+        while(tipoCompra != "carne" && tipoCompra != "vegetales" && tipoCompra != "pollo"){
+            tipoCompra = prompt("Por favor, indique un producto válido. (Carne/Vegetales/Pollo)").toLowerCase();
+        }
+        
+        if(flagMasLiviano || kilos < masLivianoKilos){
+            masLivianoKilos = kilos;
+            masLivianoTipo = tipoCompra;
+        }
+        if(tipoCompra == "carne"){
+            contadorCarne++;
+            acumKilosCarne += kilos;
+        }
+        
+    }
+
+    promedioKilos = acumKilos / 10; 
+    if(contadorCarne <= 0){
+        promedioKilosCarne = 0;
+    } else {
+        promedioKilosCarne = acumKilosCarne / contadorCarne;
+    }
+
+    alert("A) El promedio de los kilos totales es: " + promedioKilos +
+        "\nB) La compra más liviana: " + masLivianoTipo + " / " + masLivianoKilos +
+        "\nC) Cantidad de carne: " + acumKilosCarne +
+        "\nPromedio de kilos de carne: " + promedioKilosCarne);
+
 
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,101 @@
 function mostrar()
 {
-	alert("tres");
+	/*
+	En el ingreso a un viaje en avion nos solicitan nombre , edad, sexo("f" o "m") y estado civil("soltero", 
+	"casado" o "viudo") y temperatura corporal.
+		a) El nombre de la persona con mas temperatura.
+		b) Cuantos mayores de edad estan viudos
+		c) La cantidad de hombres que hay solteros o viudos.
+		d) cuantas personas de la tercera edad( mas de 60 años) , tienen mas de 38 de temperatura
+		e) El promedio de edad entre los hombres solteros.
+	*/
+
+	// Variable. 
+	let nombre;
+	let edad;
+	let sexo;
+	let estadoCivil;
+	let temperatura;
+	let respuesta;
+
+	// A) Temperatura más alta y el nombre. 
+	let temperaturaAlta; 
+	let nombreTempAlta;
+	let flagTempAlt = 1;
+
+	// B) Mayores y viudos.
+	let mayoresViudos = 0; 
+
+	// C) Hombres solteros o viudos.
+	let hombresSoltViudos = 0;
+
+	// D) Viejos con temperatura más de 38.
+	let viejosTemp38 = 0;
+
+	// E) Promedio de edad de hombres solteros.
+	let contHmbreSolteros = 0;
+	let acumEdadHmbreSolt = 0;
+	let promedEdadHmbreSolt = 0; 
+
+	// Pido datos.
+	do{
+		nombre = prompt("Ingrese su nombre y apellido.");
+		while(nombre.length == 0){
+			nombre = prompt("ERROR. Ingrese su nombre y apellido.");
+		}
+
+		edad = parseInt(prompt("Ingrese su edad."));
+		while(isNaN(edad) || edad <= 0){
+			edad = parseInt(prompt("Por favor, ingrese una edad válida."));
+		}
+
+		sexo = prompt("Ingrese su sexo. (f/m)").toLowerCase();
+		while(sexo.length == 0 || sexo != 'm' && sexo != 'f'){
+			sexo = prompt("Por favor, ingrese un sexo válido. (f/m)").toLowerCase();
+		}
+
+		estadoCivil = prompt("Ingrese su estado civil. (Soltero/Casado/Viudo)").toLowerCase();
+		while(estadoCivil.length == 0 || estadoCivil != 'soltero' && estadoCivil != 'casado' && estadoCivil != 'viudo'){
+			estadoCivil = prompt("Por favor, ingrese un número estado civil válido. (Soltero/Casado/Viudo)").toLowerCase();
+		}
+
+		temperatura = parseFloat(prompt("Ingrese su temperatura corporal."));
+		while(isNaN(temperatura) || temperatura <= 0){
+			temperatura = parseFloat(prompt("Por favor, ingrese una temperatura válida."));
+		}
+		
+		if(flagTempAlt || temperatura > temperaturaAlta){
+			nombreTempAlta = nombre;
+			temperaturaAlta = temperatura;
+		}
+		if(edad >= 18 && estadoCivil == "viudo"){
+			mayoresViudos++;
+		}
+		if(sexo == 'm' && estadoCivil == "soltero" || estadoCivil == "viudo"){
+			hombresSoltViudos++; 
+		}
+		if(edad > 60 && temperatura > 38){
+			viejosTemp38++; 
+		}
+		if(sexo == 'm' && estadoCivil == "soltero"){
+			contHmbreSolteros++;
+			acumEdadHmbreSolt += edad;
+		}
+		if(acumEdadHmbreSolt > 0){
+		promedEdadHmbreSolt = acumEdadHmbreSolt / contHmbreSolteros;
+		}
+		
+		respuesta = prompt("¿Ingresar a otra persona? (s/n)").toLowerCase();
+		while(respuesta.length == 0 || respuesta != 's' && respuesta != 'n'){
+			respuesta = prompt("Por favor, ingrese una respuesta válida. (s/n)").toLowerCase();
+		}
+
+	} while(respuesta == 's');
+
+	document.write("A) Persona con más temperatura: " + nombreTempAlta + " / " + temperaturaAlta + "°C" +
+				   "<br>B) Mayores de edad viudos: " + mayoresViudos + 
+				   "<br>C) Hombres solteros o viudos: " + hombresSoltViudos +
+				   "<br>D) Tercera edad con más de 38° de temperatura: " + viejosTemp38 +
+				   "<br>E) Promedio de edad entre hombres solteros: " + promedEdadHmbreSolt);
+
 }
